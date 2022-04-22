dd651196993c0adf9b51b4ebade62580005394e8
Adriano Grimolizzi <a.grimolizzi@gmail.com>
2022-04-22 15:07:41 +0200

    add redux

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,13 +1,31 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-
 import Task from './Task'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateTaskState } from '../lib/store'
 
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
-  const events = {
-    onPinTask,
-    onArchiveTask,
+export default function TaskList() {
+  const tasks = useSelector((state) => {
+    const tasksInOrder = [
+      ...state.taskbox.tasks.filter((t) => t.state === 'TASK_PINNED'),
+      ...state.taskbox.tasks.filter((t) => t.state !== 'TASK_PINNED'),
+    ]
+    const filteredTasks = tasksInOrder.filter(
+      (t) => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED',
+    )
+    return filteredTasks
+  })
+
+  const { status } = useSelector((state) => state.taskbox)
+
+  const dispatch = useDispatch()
+
+  const pinTask = (value) => {
+    dispatch(updateTaskState({ id: value, newTaskState: 'TASK_PINNED' }))
   }
+  const archiveTask = (value) => {
+    dispatch(updateTaskState({ id: value, newTaskState: 'TASK_PINNED' }))
+  }
+
   const LoadingRow = (
     <div className="loading-item">
       <span className="glow-checkbox" />
@@ -19,7 +37,7 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     </div>
   )
 
-  if (loading) {
+  if (status === 'loading') {
     return (
       <div className="list-items" data-testid="loading" key={'loading'}>
         {LoadingRow}
@@ -33,35 +51,27 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   }
 
   if (tasks.length === 0) {
-    return <div className="list-items" key={"empty"} data-testid="empty">
-      <div className='wrapper-message'>
-        <span className='icon-check' />
-        <div className='title-message'>You have no tasks</div>
-        <div className='subtitle-message'>Sit back and relax</div>
+    return (
+      <div className="list-items" key={'empty'} data-testid="empty">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <div className="title-message">You have no tasks</div>
+          <div className="subtitle-message">Sit back and relax</div>
+        </div>
       </div>
-    </div>
+    )
   }
 
-  const tasksInOrder = [
-    ...tasks.filter((t) => t.state === "TASK_PINNED"),
-    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
-  ]
-
   return (
-    <div className="list-items">
-      {tasksInOrder.map((task) => (
-        <Task key={task.id} task={task} {...events} />
+    <div className="list-items" data-testid="success" key={'success'}>
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          onPinTask={(task) => pinTask(task)}
+          onArchiveTask={(task) => archiveTask(task)}
+        />
       ))}
     </div>
   )
-}
-
-TaskList.protoTypes = {
-  loading: PropTypes.bool,
-  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
-  onPinTask: PropTypes.func,
-  onArchiveTask: PropTypes.func,
-}
-TaskList.defaultProps = {
-  loading: false,
 }
